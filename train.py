c89a4980acfce61fc5865a849c91b546dffe9cbc
SK <SilverKnightVGM@users.noreply.github.com>
2020-01-15 22:14:49 -0500

    Allowed soft-placement, so that TF doesn't throw an error if you specify a GPU number

--- train.py
+++ train.py
@@ -165,7 +165,7 @@ def train(config):
     nat_summaries = tf.summary.merge_all('nat')
     
 
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True, allow_soft_placement=True)) as sess:
 
       # initialize data augmentation
       if config.training.data_augmentation:
